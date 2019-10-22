1d22d62848938a6f70ba2bc5001b025f510c3433
Jason Morris <jason@roundtablelaw.ca>
2019-10-22 05:04:12 +0000

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # docassemble-blawx
 A module for connecting Docassemble interviews to Blawx.com
+
+## Installation
+Go into the packages section of your docassemble server and enter the github address for
+this package.  See the [docassemble documentation](https://docassemble.org/docs)
+for details.
+
+## Usage
+1. Include docassemble.blawx as a module in your interview.
+```
+modules:
+  - docassemble.blawx
+```
+2. Create a object with the type Blawx in your interview.
+```
+objects:
+  - reasoner: Blawx
+```
+3. Save your Blawx source code to a file, and upload that file to the static folder of 
+   your docassemble server.
+   See the [docassemble documentation](https://docassemble.org/docs) for details.
+4. Call the ask() method of the Blawx object you created.
+```
+  answer = reasoner.ask('filename.blawx')
+```
+5. Use the output of the ask() method to read the answer from the Blawx.com Reasoner.
+
+## The Docassemble Data Structure
+When you use this module, the interview data is sent to the Blawx Reasoner, where it is
+converted for use in Blawx Code.  The docassemble interview data will be converted
+into a "data dictionary", with "data" as the name of the root object.
+
+For example, if you have a variable named "foo", that will appear in Blawx as an object
+named "data", with a data property named "foo", set to the value of "foo".
+
+Nested objects, such as docassemble's DAList and elements, will be represented as
+nested data dictionaries. Only the root of the dictionary is a named object.
+
+## The Blawx Response Structure
+Blawx's response is in the following format:
+```
+{
+  'main': 'Yes' (or 'No'),
+  'answers': {
+    '1': {
+      'variable': value,
+      ...
+    }
+    ...
+  }
+}
+```
+If the query is a yes/no query, only 'main' will be returned. If the query is a search
+and no results are returned, only 'main' will be returned with a value of 'No'.
+If it is a search and there are results, 'main' and 'answers' will be included.
+
+  
+
+See the Blawx.com documentation for more information on how to write Blawx code that is
+able.
--- setup.py
+++ setup.py
@@ -43,13 +43,13 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.blawx',
-      version='',
-      description=(''),
-      long_description=u'# docassemble-blawx\r\nA module for connecting Docassemble interviews to Blawx.com\r\n',
+      version='0.0.1',
+      description=('A module to allow docassemble interviews to run Blawx code on the Blawx Reasoner over API.'),
+      long_description=u'# docassemble-blawx\r\nA module for connecting Docassemble interviews to Blawx.com\r\n\r\n## Installation\r\nGo into the packages section of your docassemble server and enter the github address for\r\nthis package.  See the [docassemble documentation](https://docassemble.org/docs)\r\nfor details.\r\n\r\n## Usage\r\n1. Include docassemble.blawx as a module in your interview.\r\n```\r\nmodules:\r\n  - docassemble.blawx\r\n```\r\n2. Create a object with the type Blawx in your interview.\r\n```\r\nobjects:\r\n  - reasoner: Blawx\r\n```\r\n3. Save your Blawx source code to a file, and upload that file to the static folder of \r\n   your docassemble server.\r\n   See the [docassemble documentation](https://docassemble.org/docs) for details.\r\n4. Call the ask() method of the Blawx object you created.\r\n```\r\n  answer = reasoner.ask(\'filename.blawx\')\r\n```\r\n5. Use the output of the ask() method to read the answer from the Blawx.com Reasoner.\r\n\r\n## The Docassemble Data Structure\r\nWhen you use this module, the interview data is sent to the Blawx Reasoner, where it is\r\nconverted for use in Blawx Code.  The docassemble interview data will be converted\r\ninto a "data dictionary", with "data" as the name of the root object.\r\n\r\nFor example, if you have a variable named "foo", that will appear in Blawx as an object\r\nnamed "data", with a data property named "foo", set to the value of "foo".\r\n\r\nNested objects, such as docassemble\'s DAList and elements, will be represented as\r\nnested data dictionaries. Only the root of the dictionary is a named object.\r\n\r\n## The Blawx Response Structure\r\nBlawx\'s response is in the following format:\r\n```\r\n{\r\n  \'main\': \'Yes\' (or \'No\'),\r\n  \'answers\': {\r\n    \'1\': {\r\n      \'variable\': value,\r\n      ...\r\n    }\r\n    ...\r\n  }\r\n}\r\n```\r\nIf the query is a yes/no query, only \'main\' will be returned. If the query is a search\r\nand no results are returned, only \'main\' will be returned with a value of \'No\'.\r\nIf it is a search and there are results, \'main\' and \'answers\' will be included.\r\n\r\n  \r\n\r\nSee the Blawx.com documentation for more information on how to write Blawx code that is\r\nable.',
       long_description_content_type='text/markdown',
       author='Jason Morris',
       author_email='jason@roundtablelaw.ca',
-      license='',
+      license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
